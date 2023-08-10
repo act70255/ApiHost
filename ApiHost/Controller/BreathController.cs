167fb8817588bc95f16062479aab45908b56bf3e
james.lin <james.lin@collaborate.tw>
2023-08-10 15:35:57 +0800

    加入專案檔案。

---
+++ ApiHost/Controller/BreathController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ApiHost.Controller
+{
+    public class BreathController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            return Json(new { SystemTime = DateTime.Now });
+        }
+        [HttpPost]
+        public IHttpActionResult Post(string req)
+        {
+            return Json(new { HostName = Environment.MachineName});
+        }
+    }
+}
---
+++ ApiHost/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiHost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseAddress = "http://localhost:9000/";
+
+            // Start OWIN host 
+            using (WebApp.Start<StartUp>(url: baseAddress))
+            {
+                #region Testing...
+                HttpClient client = new HttpClient();
+
+                var response = client.GetAsync(baseAddress + "api/Breath/Get").Result;
+
+                Console.WriteLine(response);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                Console.ReadLine(); 
+                #endregion
+            }
+        }
+    }
+}
